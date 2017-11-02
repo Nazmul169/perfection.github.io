ff7df50705506f505b01581a2499e283e322b332
Nazmul169 <hassannazmul169@gmail.com>
2017-11-03 02:01:16 +0600

    HomePage

---
+++ Homepage/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+  $(".home-area-slide").owlCarousel({
+    items: 1,
+    margin: 30,
+    loop: true,
+    // autoplay: true,
+    autoplayTimeout: 2000,
+    dots: true,
+    // nav: true,
+    // navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+  });
+});
