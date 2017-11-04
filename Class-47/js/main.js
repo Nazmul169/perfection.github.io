29c684a69682750f8b8032455c9c74c4452908a0
Nazmul169 <hassannazmul169@gmail.com>
2017-11-05 02:11:16 +0600

    class-47

---
+++ Class-47/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+  $(".slider-area").owlCarousel({
+    items: 1,
+    loop: true,
+    // autoplay: true,
+    autoplayTimeout: 2000,
+    dots: true,
+    
+  });
+});
